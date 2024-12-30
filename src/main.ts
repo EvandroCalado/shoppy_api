18d2f2ca508fc68b2dc9aed0f20eeacb6d0ad032
Evandro Calado <evandrocalado07@gmail.com>
2024-12-30 20:55:53 -0300

    feat: initialize NestJS project with user module, DTOs, and basic validation

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useLogger(app.get(Logger));
+
+  await app.listen(app.get(ConfigService).getOrThrow('PORT'));
+}
+bootstrap();
